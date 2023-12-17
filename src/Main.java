ce4c2356dc8adaae6169407052ce8cbe8caf5ba0
zxc777zxc <230811@astanait.edu.kz>
2023-12-17 15:45:50 +0600

    add scanner

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
-
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        
+        Scanner zxc = new Scanner(System.in);
 
-        int a = 15;
-        int b = 15;
+        int a = zxc.nextInt();
+        int b = zxc.nextInt();
         System.out.println(a+b);
     }
 }
